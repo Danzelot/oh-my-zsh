14bed37258241710a8485cc3575baca115fc0b9d
Joern Dietze <j.dietze@posteo.de>
2016-06-28 15:16:34 +0200

    Shifted alias from example.zsh to alias.zsh

--- custom/example.zsh
+++ custom/example.zsh
@@ -8,17 +8,3 @@
 # brainstormr=~/Projects/development/planetargon/brainstormr
 # cd $brainstormr
 #
-alias -s out=vim
-alias -s tex=vimlatex
-alias -s jl=vim
-alias -s pdf=evince
-alias qc='cd /home/jd/devel/qcmine/'
-alias ql='cd /home/jd/.julia/v0.4/QuantumLab/'
-alias ma='cd /home/jd/Documents/ma'
-
-alias tmux='tmux -f ~/.tmux_theme_airline.conf'
-# alias tor='ibus exit; /usr/local/sbin/tor-browser_de/start-tor-browser &'
-alias vless='/usr/share/vim/vim74/macros/less.sh -R'
-alias vi="vim"
-alias gvimlatex="gvim --servername latex -c 'set spell' -c 'set spelllang=en_gb'"
-alias vimlatex="vim -c 'set spell' -c 'set spelllang=en_gb'"
